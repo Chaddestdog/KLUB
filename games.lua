d23bad315c6132ca89849b1256eeb917704ba80e
Chaddestdog <205866822+Chaddestdog@users.noreply.github.com>
2025-10-13 00:39:59 -0400

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,1 +1,8 @@
-
+return {
+    ["72920620366355"] = {
+        name = "Operation One",
+        pid = 0,
+        load = loadstring(game:HttpGet("https://github.com/Chaddestdog/KLUB/blob/main/Operation%20One/main.lua")),
+        bypass_ver = "1.0",
+    },
+};
