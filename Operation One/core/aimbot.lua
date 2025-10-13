8fccf438ccdb8d1dde7931d0fce2f052b11df9ad
Chaddestdog <205866822+Chaddestdog@users.noreply.github.com>
2025-10-12 22:45:36 -0400

    Update aimbot.lua

--- Operation One/core/aimbot.lua
+++ Operation One/core/aimbot.lua
@@ -1,18 +1,16 @@
 local aimbot = {};
+local user_input_service:  UserInputService;
 
-local Players = game:GetService("Players")
-local user_input_service:  UserInputService = game:GetService("UserInputService");
-
-rawset(aimbot, "aim_at", newcclosure(function(pos: Vector3, smoothing: number)
-    local mouse_sen = (user_input_service.MouseDeltaSensitivity * smoothing);
+rawset(aimbot, "aim_at", newcclosure(function(pos: Vector3)
+    if (user_input_service.MouseBehavior == Enum.MouseBehavior.Default) then return end;
+    local mouse_sen = (user_input_service.MouseDeltaSensitivity);
     local mouse_pos = user_input_service:GetMouseLocation();
     local aim_pos = to_view_point(pos);
     mousemoverel((aim_pos.X - mouse_pos.X) / mouse_sen, (aim_pos.Y - mouse_pos.Y) / mouse_sen);
 end));
 
-
 aimbot.init = function()
-    --user_input_service = get_service("UserInputService");
+    user_input_service = get_service("UserInputService");
 end;
 
 return aimbot;
