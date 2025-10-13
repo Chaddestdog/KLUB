97a0efc582761fb19d862b04d02b6c6f3335ef40
Chaddestdog <205866822+Chaddestdog@users.noreply.github.com>
2025-10-12 23:35:32 -0400

    Update player esp.lua

--- Operation One/core/player esp.lua
+++ Operation One/core/player esp.lua
@@ -5,6 +5,11 @@ local esp_ran = {};
 local players: Players;
 local run_service: RunService;
 local camera = cloneref(workspace.CurrentCamera);
+local settings = {
+    health_bar = false
+};
+
+
 
 rawset(player_esp, "set_player_esp", newcclosure(function(character: Model)
     task.wait(0.5);
@@ -41,22 +46,32 @@ rawset(player_esp, "set_player_esp", newcclosure(function(character: Model)
         local point, on = to_view_point(torso.CFrame.Position);
         if (on) then
             for i, v in next, (esp_ran) do v(has_esp[character]) end;
-            health_bar_inner.Visible = true;
-            health_bar_outer.Visible = true;
+           
 
             local cf_mid, size = character:GetBoundingBox();
             local bottom_right = to_view_point((CFrame.new(cf_mid.Position, camera.CFrame.Position) * CFrame.new(-size.X / 2, -size.Y / 2, 0)).Position);
             local bottom_left = to_view_point((CFrame.new(cf_mid.Position, camera.CFrame.Position) * CFrame.new(size.X / 2, -size.Y / 2, 0)).Position);
-            local health = math.clamp((humanoid.Health / humanoid.MaxHealth), 0, 1);
-                    
-            health_bar_outer.Size = Vector2.new(bottom_left.X - bottom_right.X, 3);
-            health_bar_outer.Position = Vector2.new(bottom_right.X, bottom_left.Y);
+            
+            if (settings.health_bar) then
+                health_bar_inner.Visible = true;
+                health_bar_outer.Visible = true;
 
-            health_bar_inner.Size = Vector2.new((((bottom_left.X - bottom_right.X) + 2) * health), 1);
-            health_bar_inner.Position = Vector2.new(health_bar_outer.Position.X - 1, bottom_left.Y + 1);
-            health_bar_inner.Color =  Color3.new(1, 0, 0):Lerp(Color3.new(0, 1, 0), health);
+                local health = math.clamp((humanoid.Health / humanoid.MaxHealth), 0, 1);
 
-            size, cf_mid, bottom_right, bottom_left, health = nil, nil, nil, nil, nil;
+                health_bar_outer.Size = Vector2.new(bottom_left.X - bottom_right.X, 3);
+                health_bar_outer.Position = Vector2.new(bottom_right.X, bottom_left.Y);
+    
+                health_bar_inner.Size = Vector2.new((((bottom_left.X - bottom_right.X) + 2) * health), 1);
+                health_bar_inner.Position = Vector2.new(health_bar_outer.Position.X - 1, bottom_left.Y + 1);
+                health_bar_inner.Color =  Color3.new(1, 0, 0):Lerp(Color3.new(0, 1, 0), health);
+
+                health = nil;
+            else
+                health_bar_inner.Visible = false;
+                health_bar_outer.Visible = false;
+            end;
+           
+            size, cf_mid, bottom_right, bottom_left = nil, nil, nil, nil;
         else
             health_bar_inner.Visible = false;
             health_bar_outer.Visible = false;
@@ -88,6 +103,8 @@ end));
 rawset(player_esp, "get_player_from_has_esp", newcclosure(function(character: Model)
     return has_esp[character];
 end));
+
+rawset(player_esp, "esp_player_settings", settings);
 
 player_esp.init = function()
     players = get_service("Players");
