42354012961b1b34edbc6634267a297e7c07d33a
Chaddestdog <205866822+Chaddestdog@users.noreply.github.com>
2025-10-13 01:27:53 -0400

    Update player esp.lua

--- Operation One/core/player esp.lua
+++ Operation One/core/player esp.lua
@@ -2,11 +2,14 @@ local player_esp = {};
 
 local has_esp = {};
 local esp_ran = {};
-local players: Players;
-local run_service: RunService;
+local core_gui:     CoreGui;
+local players:      Players;
+local run_service:  RunService;
 local camera = cloneref(workspace.CurrentCamera);
 local settings = {
-    health_bar = false
+    health_bar = false,
+    skelton = false,
+    skelton_color = Color3.fromRGB(255, 255, 255),
 };
 
 
@@ -42,6 +45,15 @@ rawset(player_esp, "set_player_esp", newcclosure(function(character: Model)
         health_bar_outer.ZIndex = 1;
     end;
 
+    local skeleton = Instance.new("WireframeHandleAdornment", core_gui) do
+        skeleton.Color3 = Color3.new(1, 1, 1);
+        skeleton.Visible = true;
+        skeleton.AlwaysOnTop = true;
+        skeleton.Adornee = workspace;
+        skeleton.Thickness = 1;
+        skeleton.ZIndex = 5;
+    end;
+
     c1 = run_service.RenderStepped:Connect(function(delta: number)
         local point, on = to_view_point(torso.CFrame.Position);
         if (on) then
@@ -51,7 +63,8 @@ rawset(player_esp, "set_player_esp", newcclosure(function(character: Model)
             local cf_mid, size = character:GetBoundingBox();
             local bottom_right = to_view_point((CFrame.new(cf_mid.Position, camera.CFrame.Position) * CFrame.new(-size.X / 2, -size.Y / 2, 0)).Position);
             local bottom_left = to_view_point((CFrame.new(cf_mid.Position, camera.CFrame.Position) * CFrame.new(size.X / 2, -size.Y / 2, 0)).Position);
-            
+            local head_offset = (character.head.CFrame * -Vector3.new(0, (character.head.Size.Y / 2), 0));
+
             if (settings.health_bar) then
                 health_bar_inner.Visible = true;
                 health_bar_outer.Visible = true;
@@ -70,9 +83,18 @@ rawset(player_esp, "set_player_esp", newcclosure(function(character: Model)
                 health_bar_inner.Visible = false;
                 health_bar_outer.Visible = false;
             end;
+
+            if (settings.skelton) then
+                skeleton:Clear();
+                skeleton.Color3 = settings.skelton_color;
+                skeleton:AddLines({character.head.Position, character.torso.Position, head_offset, character.shoulder2.Position, character.shoulder2.Position, character.arm2.Position, head_offset, character.shoulder1.Position, character.shoulder1.Position, character.arm1.Position, character.torso.Position, character.hip2.Position, character.hip2.Position, character.leg2.Position, character.torso.Position, character.hip1.Position, character.hip1.Position, character.leg1.Position});
+            else
+                skeleton:Clear();
+            end;
            
             size, cf_mid, bottom_right, bottom_left = nil, nil, nil, nil;
         else
+            skeleton:Clear();
             health_bar_inner.Visible = false;
             health_bar_outer.Visible = false;
         end;
@@ -109,6 +131,7 @@ rawset(player_esp, "esp_player_settings", settings);
 player_esp.init = function()
     players = get_service("Players");
     run_service = get_service("RunService");
+    core_gui = get_service("CoreGui");
 end;
 
 return player_esp;
