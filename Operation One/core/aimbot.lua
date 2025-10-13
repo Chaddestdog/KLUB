b59e611fe855d1268ff4d68d390945c9b3aec4ec
Chaddestdog <205866822+Chaddestdog@users.noreply.github.com>
2025-10-12 20:41:19 -0400

    Create aimbot.lua

---
+++ Operation One/core/aimbot.lua
@@ -0,0 +1,18 @@
+local aimbot = {};
+
+local user_input_service:  UserInputService = game:GetService("UserInputService");
+
+rawset(aimbot, "aim_at", newcclosure(function(pos: Vector3)
+    local aim_pos = to_view_point(pos);
+    local mouse_sen = user_input_service.MouseDeltaSensitivity;
+
+    mousemoverel(aim_pos.X / mouse_sen, aim_pos.Y / mouse_sen);
+end));
+
+
+aimbot.init = function()
+    --user_input_service = get_service("UserInputService");
+
+end;
+
+return aimbot;
