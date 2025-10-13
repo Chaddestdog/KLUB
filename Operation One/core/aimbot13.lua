626f0f3e0971a3beac1fa5d4e4553a4a350bb8c5
Chaddestdog <205866822+Chaddestdog@users.noreply.github.com>
2025-10-13 15:27:01 -0400

    Update and rename aimbot12.lua to aimbot13.lua

--- Operation One/core/aimbot13.lua
+++ Operation One/core/aimbot13.lua
@@ -3,7 +3,9 @@ local user_input_service:  UserInputService;
 local run_service:         RunService;
 local players:             Players;
 local camera:              Camera = cloneref(workspace.CurrentCamera);
-
+local start = 0;
+local rot = Vector2.new();
+local running = false;
 
 local settings = {
     enabled = false,
@@ -69,26 +71,38 @@ aimbot.init = function()
     user_input_service = get_service("UserInputService");
     run_service = get_service("RunService");
     players = get_service("Players");
+
     on_esp_ran(function(has_esp: table, point: Vector2)
         local player, closest, screen_pos, aim_part = find_closest();
-        if (not (player and closest)) then return end;
-        if (user_input_service.MouseBehavior == Enum.MouseBehavior.Default) then return end;
-        local start = 0;
-        local rot = Vector2.new();
-        local c1; c1 = run_service.RenderStepped:Connect(function(delta: number)
-            start += (delta * 1000);
-            local lerp = math.clamp(start / settings.smoothing, 0, 1);
-            local base_cfrmae = camera.CFrame:Lerp(CFrame.lookAt(camera.CFrame.Position, aim_part.CFrame.Position, Vector3.new(0, 1, 0)), (1 - (1 - lerp) ^ 2));
-            rot += (user_input_service:GetMouseDelta() * 0.0005);
+        if (not (player and closest and aim_part)) then
+            running = false;
+            return;
+        end;
 
-            camera.CFrame = base_cfrmae * CFrame.Angles(0, -rot.X, 0) * CFrame.Angles(-rot.Y, 0, 0);
+        if (user_input_service.MouseBehavior == Enum.MouseBehavior.Default) then
+            running = false;
+            return;
+        end;
 
-            if (lerp >= 1) then
-                start = 0;
-                rot = Vector2.new();
-                c1:Disconnect();
-            end;
-        end);
+        if (not running) then
+            start = 0;
+            rot = Vector2.zero;
+            running = true;
+        end;
+
+        start += (run_service.RenderStepped:Wait() * 1000);
+        local lerp = math.clamp((start / settings.smoothing), 0, 1);
+
+        local goal_cframe = CFrame.lookAt(camera.CFrame.Position, aim_part.CFrame.Position, Vector3.new(0, 1, 0));
+        local base_cframe = camera.CFrame:Lerp(goal_cframe, (1 - (1 - lerp) ^ 2));
+
+        rot += (user_input_service:GetMouseDelta() * 0.0005);
+        camera.CFrame = (base_cframe * CFrame.Angles(0, -rot.X, 0) * CFrame.Angles(-rot.Y, 0, 0));
+
+        if (lerp >= 1) then
+            start = 0;
+            running = true;
+        end;
     end);
 end;
 
