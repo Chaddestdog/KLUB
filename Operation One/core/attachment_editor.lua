9c9befdb97fa8173b1dc5adec4b8c4de042e0647
Chaddestdog <205866822+Chaddestdog@users.noreply.github.com>
2025-10-16 13:53:54 -0400

    Create attachment_editor.lua

---
+++ Operation One/core/attachment_editor.lua
@@ -0,0 +1,50 @@
+local attachment_editor = {};
+local attachment_modules = {};
+local replicated_storage:  ReplicatedStorage;
+local players:             Players;
+local local_player:        Player;
+local viewmodels:          Folder = workspace.Viewmodels;
+
+local setings = {
+    skin = "Default",
+};
+
+
+local get_local_player_gun = function()
+    local gun, local_player_char = nil, viewmodels:FindFirstChild("Viewmodels/" .. local_player.Name);
+    if (not local_player_char) then return end;
+    for i, v: Instance in (local_player_char:GetChildren()) do
+        if (v:FindFirstChild("Gun")) then
+            gun = {instance = v};
+            break;
+        end;
+    end;
+    return gun;
+end;
+
+rawset(attachment_editor, "set_skin", newcclosure(function()
+    local gun = get_local_player_gun();
+    if (not gun) then return end;
+
+    if (settings.skin == "Default") then
+        attachment_modules["Skin"].remove(attachment_modules["Skin"], gun);
+    else
+        attachment_modules["Skin"].apply(require(attachment_modules["Skin"].module[settings.skin]), gun);
+    end;
+
+end));
+
+rawset(attachment_editor, "attachment_editor_settings", settings);
+
+attachment_editor.init = function()
+    replicated_storage = get_service("ReplicatedStorage");
+    players = get_service("Players");
+    local_player = players.LocalPlayer;
+
+    for i, v in next, (replicated_storage.Modules.Items.Item.Attachment:GetChildren()) do
+        attachment_modules[v.name] = require(v);
+        attachment_modules[v.name].module = v;
+    end;
+end;
+
+return attachment_editor;
